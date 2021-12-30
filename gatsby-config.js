8b56cd4261f38df6cc19366df750f698851dc16b
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2021-12-29 18:52:06 -0800

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://myfirstgatsbysite00346.gatsbyjs.io/",
     title: "My Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    }
   ],
 };
